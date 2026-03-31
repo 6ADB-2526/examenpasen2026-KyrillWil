b679c633ccf869eb9e7217163eb33e65bb942f5f
Kyrill Willems <kyrill.willems@student.tsaam.be>
2026-03-31 11:56:01 +0200

    commit3

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,25 @@ import RegistrationForm from "./components/RegistrationForm";
 import UserList from "./components/UserList/UserList";
 
 interface User {
-  id: number;
   naam: string;
 }
 
 export default function App() {
   //statische lijst met users die al geregistreerd waren
   const GeregistreerdePersonen = ["Karel", "Els", "Piet"];
-  //dynamische lijst voor users toe te voegen(voornaam is even weggelaten)
-  const [newUser, setNewUser] = useState([{ id: 0, naam: "" }]);
-  function voegToe(formData: User) {
-    setNewUser({ ...newUser, formData });
-    // const newList = setNewUser([{ ...newUser, formData }]);
-    // return newList;
+  //dynamische lijst voor users toe te voegen(id en voornaam is even weggelaten hier en ook uit UserList omdat het eerst met naam moet werken)
+  const [newUser, setNewUser] = useState([{ naam: "" }]);
+  //functie die de nieuwe namen toevoegd aan de lijst
+  function voegToe(name: User) {
+    const newList = name;
+    setNewUser({ ...newUser, name });
+    return newList;
   }
   return (
     <div>
       <RegistrationForm onRegister={voegToe} />
       <hr />
-      <UserList registeredUsers={GeregistreerdePersonen} users={newUser} />
+      <UserList registeredUsers={GeregistreerdePersonen} users={newList} />
     </div>
   );
 }
